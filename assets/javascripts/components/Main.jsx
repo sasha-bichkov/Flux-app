890fa4b62a5796d285111255ef42c9c32787b7ad
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-01-09 08:49:12 -0500

    Material-ui 'Hello, world'

---
+++ assets/javascripts/components/Main.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'; 
+
+import Card      from 'material-ui/lib/card/card';
+import CardTitle from 'material-ui/lib/card/card-title';
+
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main id="main">
+        <Card>
+          <CardTitle title="Hello, world!" />
+        </Card>
+      </main>
+    )
+  }
+};
