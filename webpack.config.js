ce144bca3c19b32e863cd377f6e3bd56de6fe861
sasha-bichkov <aleksandar.bichkov@gmail.com>
2016-01-10 05:17:03 -0500

    Added webpack-dev-server and react-hot-reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
-module.exports = {
-  entry : './assets/javascripts/index',
-  output : {
-    path: './build',
-    filename: 'bundle.js'
-  },
+var path = require('path');
+var webpack = require('webpack');
 
+module.exports = {
+  entry : [
+    'webpack/hot/dev-server',
+    './assets/javascripts/index'
+  ],
+  output : {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/build'
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
       }
     ]
   }
